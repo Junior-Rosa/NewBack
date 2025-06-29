1fc4fff3baae9535278d6f6aac8f45ee6a1bd10f
Pablo Gascón <pablogasconiel445@gmail.com>
2025-06-29 17:40:17 -0300

    feat: implement dynamic employee department count

--- src/comercial/views/employee.py
+++ src/comercial/views/employee.py
@@ -2,15 +2,43 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, CreateView
 from django.urls import reverse_lazy
 from django.contrib import messages
-from ..models import Employee
+from ..models import Employee, Department
 from ..forms import EmployeeCreateForm
-
+from django.db.models import Q, Count
 
 class EmployeeListView(LoginRequiredMixin, ListView):
     model = Employee
     template_name = 'employees/employee_list.html'
     context_object_name = 'employees'
     paginate_by = 20
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        department = self.request.GET.get('department')
+        search = self.request.GET.get('search')
+
+        if department:
+            queryset = queryset.filter(departament_id=department)
+
+        if search:
+            queryset = queryset.filter(
+                Q(user__first_name__icontains=search) |
+                Q(user__last_name__icontains=search) |
+                Q(user__username__icontains=search)
+            )
+
+        return queryset
+
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Lista de Funcionários'
+        context['subtitle'] = 'Gerencie os funcionários da empresa'
+        context['total_departments_count'] = Department.objects.count()
+        # Pegar departamentos mais a quantidade de funcionários em cada um
+        context['departments'] = Department.objects.all().annotate(employee_count=Count('employee'))
+        context['total_delivery_employees'] = Employee.objects.filter(departament__name='Entrega').count()
+        return context
 
 class EmployeeCreateView(LoginRequiredMixin, CreateView):
     model = Employee
